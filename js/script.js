8286de954529b192ffff67ebee3b101f78a87522
priscahlimo <priscahlimoh@gmail.com>
2022-01-03 21:12:02 +0300

    add show and hide jQuery

--- js/script.js
+++ js/script.js
@@ -16,35 +16,35 @@ $(document).ready(function(){
 
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
     
-    var projects =[{name:"first",info:"description1"},{name:"second",info:"description2"},{name:"third",info:"description3"},{name:"fourth",info:"description4"},{name:"fifth",info:"description5"},{name:"sixth",info:"description6"},{name:"seventh",info:"description7"},{name:"eighth",info:"description8"}];
+//     var projects =[{name:"first",info:"description1"},{name:"second",info:"description2"},{name:"third",info:"description3"},{name:"fourth",info:"description4"},{name:"fifth",info:"description5"},{name:"sixth",info:"description6"},{name:"seventh",info:"description7"},{name:"eighth",info:"description8"}];
     
-    projects.forEach(function(project){
-        $("#" +project.name).mouseover(function(){
-            $("#"+project.info).show();
-        }).mouseout(function(){
-            $("#"+project.info).hide();
-        });
-    })
-});
+//     projects.forEach(function(project){
+//         $("#" +project.name).mouseover(function(){
+//             $("#"+project.info).show();
+//         }).mouseout(function(){
+//             $("#"+project.info).hide();
+//         });
+//     })
+// });
 
 
 
 
-function submitForm(){
-    var user = document.getElementById("user").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
+// function submitForm(){
+//     var user = document.getElementById("user").value;
+//     var email = document.getElementById("email").value;
+//     var message = document.getElementById("message").value;
     
-    if(email == null){
-        alert("Please enter a valid email")
-    }else if(user == null){
-        alert("Please enter your name")
-    }else if(message == null){
-        alert("Please enter your message")
-    }else{
-        alert( user + " we have received your message. Thank you for reaching out to us.")
-    }
-}
+//     if(email == null){
+//         alert("Please enter a valid email")
+//     }else if(user == null){
+//         alert("Please enter your name")
+//     }else if(message == null){
+//         alert("Please enter your message")
+//     }else{
+//         alert( user + " we have received your message. Thank you for reaching out to us.")
+//     }
+// }
 
